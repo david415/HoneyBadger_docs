705afa779a578ba78732f7f77313ec69480816c6
David Stainton <dstainton415@gmail.com>
2016-02-15 11:09:49 +0100

    Attempt to use alabaster theme with color changes

--- source/conf.py
+++ source/conf.py
@@ -98,14 +98,14 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
     "bgcolor":"black",
-    "fgcolor":"white"
+    "body_text":"gray_3"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
